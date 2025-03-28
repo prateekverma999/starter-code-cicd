035f13cdfa17e3dc8980400df576e60d3b2d9d92
prateek verma <prateekverma999@live.com>
2025-03-29 01:38:28 +0530

    apptest.jsx fail code added

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -18,5 +18,6 @@ test('contains an h1 element', () => {
 test('contains an h2 element', () => {
   const { container } = render(<App />);
   const h2Element = container.querySelector('h2');
-  expect(h2Element).toBeInTheDocument();
+  // expect(h2Element).toBeInTheDocument(); 
+  expect(h2Element).not.toBeInTheDocument(); //to do workflow fail.
 });
